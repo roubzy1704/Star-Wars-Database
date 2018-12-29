8ca167f9e5c24ad90a384b9f63dc8a2239816524
roubzy1704 <roubzy1704@yahoo.com>
2018-12-28 23:40:51 -0600

    Updates

---
+++ precache-manifest.7fab3bc244dbf949407c3cfc8228c015.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ade91cb0bb7c69696a7df18125ae82cb",
+    "url": "/Star-Wars-Database/static/media/Death Star.ade91cb0.otf"
+  },
+  {
+    "revision": "c61fa28668d409b76da6",
+    "url": "/Star-Wars-Database/static/js/runtime~main.c61fa286.js"
+  },
+  {
+    "revision": "ab483c07e966e364e7dd",
+    "url": "/Star-Wars-Database/static/js/main.ab483c07.chunk.js"
+  },
+  {
+    "revision": "dd1eeda6d9da70734197",
+    "url": "/Star-Wars-Database/static/js/1.dd1eeda6.chunk.js"
+  },
+  {
+    "revision": "ab483c07e966e364e7dd",
+    "url": "/Star-Wars-Database/static/css/main.c323ab2c.chunk.css"
+  },
+  {
+    "revision": "dd1eeda6d9da70734197",
+    "url": "/Star-Wars-Database/static/css/1.324cb6dc.chunk.css"
+  },
+  {
+    "revision": "4e1db7f2b99d555f106c2b7b6faac6d1",
+    "url": "/Star-Wars-Database/index.html"
+  }
+];
